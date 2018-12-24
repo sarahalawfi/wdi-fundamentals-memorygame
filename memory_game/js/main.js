ebe02f226c8bcc9306a831781eb38aa7b5f6bf14
sara saleh alofi <sara@saras-MacBook-Pro.local>
2018-12-24 14:43:30 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,42 @@
-//console.log("Up and running!");
 
-/*var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";*/
-
+/*if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}*/
 
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var checkForMatch = function(){
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+}
+
+};
+
+
+
+var flipCard = function(cardId){
+
+console.log("User flipped"+" "+cards[cardId]);
+cardsInPlay.push(cards[cardId]);
 
 if(cardsInPlay.length===2){
-	if(cardsInPlay[0]===cardsInPlay[1]){
-		alert("You found a match!");
-	}else{
-		alert("Sorry, try again.");}
-	
+	checkForMatch();
 }
+
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
+
