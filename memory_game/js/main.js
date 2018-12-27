cf5a4fa40dfd40e8fb16e98e011a45ae78e4d185
sara saleh alofi <sara@saras-MacBook-Pro.local>
2018-12-27 12:25:10 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,4 @@
 
-/*if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
-} else {
-console.log("Sorry, try again.");
-}*/
 
 var cards = [
 {
@@ -41,21 +36,46 @@ alert("Sorry, try again.");
 
 
 
-var flipCard = function(cardId){
+var flipCard = function(){
+
+var cardId = this.getAttribute('data-id');
 
 console.log("User flipped"+" "+cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 
+this.setAttribute('src',cards[cardId].cardImage);
+
 if(cardsInPlay.length===2){
 	checkForMatch();
+}
+};
+
+var createBoard = function(){
+for(var i=0; i<cards.length; i++){
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src',"images/back.png");
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
 }
 
 };
 
-flipCard(0);
-flipCard(2);
+
+createBoard();
+
+// for reset game it will reloading the page
+var resetGame = function(){
+	window.location.reload();
+}
+
+var reset = document.getElementById('Reset');
+reset.addEventListener('click',resetGame);
+
+
+
 
 
 
